a59bc0f34500e6abb89bd3839fb2c46874533956
camilo-camargo <camilocamargo49@gmail.com>
2024-03-08 19:53:28 -0500

    - Add react native macOs and structure in src folder.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+
+export default function App(){
+  return(
+  <View style={{
+      minHeight: "100%",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>Leads1LLC</Text>
+  </View>
+  );
+}
